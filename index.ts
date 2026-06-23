70fee9fc1362c8e54ee64e0ba4b17c4ff36a3cbe
ansariarifdev <ansariarif.dev@gmail.com>
2026-06-23 14:59:17 +0530

    added / route

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import productRoutes from "./routes/products";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "this is the Home page. For products, go to /products endpoint"
+    });
+});
 
 app.use("/products", productRoutes);
 
